dc9011b45cfed45a99e7f6ef5507e37d0f0fc41b
Frank Quintana <ferickquintana@gmail.com>
2023-08-07 18:18:24 -0300

    componente NavBar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+// import "./navBar.css"
+
+export const NavBar = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+
+    return (
+      <div className='container'>
+        <nav className='nav'>
+          <a className='nav-logo' href='#' onClick={closeMenu} >ART-SHOP</a>
+          <div className='menu-icon' onClick={handleClick}>
+              <i className={click ? 'bi bi-x' : 'bi bi-list'} />
+          </div>
+  
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <a className='nav-link' href='#' onClick={closeMenu}>Inicio</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#' onClick={closeMenu}>Geometricos</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#' onClick={closeMenu}>Mafia</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#' onClick={closeMenu}>Astractos</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#' onClick={closeMenu}>carrito</a>
+            </li>
+          </ul>
+          
+        </nav>
+      </div>
+  )
+}
+
+export default NavBar
